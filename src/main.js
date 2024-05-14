9b7fd2d6e9a0d420068f50014f3f7c4aa2d19ac1
Oleksandr Baranov <sashok1987nk@gmail.com>
2024-05-14 19:15:50 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 
-
+// function getPokemon() {
+//     const url = 'https://pokeapi.co/api/v2/pokemon/ditto123';
+//     return fetch(url).then(response => {
+//         if(!response.ok){
+//             throw new Error('ошибка');
+//         }
+//             return response.json();
+//     })
+// }
+// getPokemon().then(data => console.log(data)).catch(error => console.log('check Url!'));
 
 
